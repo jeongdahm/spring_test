0d773f5718a1784ecf6ade45b51f85be6bb6c0f8
dong <dong@dong-PC>
2017-09-17 22:15:41 +0900

    .

---
+++ test/src/main/java/com/my/dong/vo/Test.java
@@ -0,0 +1,21 @@
+package com.my.dong.vo;
+
+public class Test {
+	private String str1 ="test1111";
+	private Integer integer1 =1111;
+	
+	public String getStr1() {
+		return str1;
+	}
+	public void setStr1(String str1) {
+		this.str1 = str1;
+	}
+	public Integer getInteger1() {
+		return integer1;
+	}
+	public void setInteger1(Integer integer1) {
+		this.integer1 = integer1;
+	}
+	
+	
+}
